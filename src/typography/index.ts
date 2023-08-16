8fc627f5da190cb78623dd42ec236150d2463aa2
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-08-16 18:18:48 -0300

    feat: add typography

---
+++ src/typography/index.ts
@@ -0,0 +1,26 @@
+export const constants = {
+  font: {
+    primary: "Source Sans Pro",
+  },
+  size: {
+    xxLarge: "20px",
+    xLarge: "18px",
+    large: "16px",
+    medium: "14px",
+    small: "12px",
+  },
+  lineHeight: {
+    xxxxLarge: "36px",
+    xxxLarge: "26px",
+    xxLarge: "24px",
+    xLarge: "21px",
+    large: "18px",
+    medium: "16px",
+    small: "14px",
+  },
+  weight: {
+    bold: 700,
+    semiBold: 600,
+    regular: 400,
+  },
+};
