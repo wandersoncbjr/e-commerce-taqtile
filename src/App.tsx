31f3724e411ce73e3c6b452a976a5910fb0bda91
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-08-21 15:50:51 -0300

    feat: add compoenent Ratting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Rating } from "./components/rate";
 
 function App() {
-  return <div></div>;
+  return (
+    <div style={{width: '10000px'}}>
+      <Rating onChange={(value) => console.log(value)} />
+    </div>
+  );
 }
 
 export default App;
