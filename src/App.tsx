5bfbf5e4d95cbfdbc03271a4e2314c0981fae6db
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-09-01 17:37:55 -0300

    fix: clear home

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { CarouselBanner } from "./components/carousel/carousel-banner";
-import { CarouselCard } from "./components/carousel/carousel-card";
 
 function App() {
-  return (
-    <>
-      <CarouselBanner />
-      <CarouselCard />
-    </>
-  );
+  return;
 }
 
 export default App;
