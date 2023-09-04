72832eba74731912e649cdd7e45703faa55d137d
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-09-04 16:55:19 -0300

    fix: rate

--- src/components/rate/index.tsx
+++ src/components/rate/index.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Rate } from "rsuite";
 import "rsuite/dist/rsuite.min.css";
+import styled from "styled-components";
 
 interface RatingProps {
   defaultValue?: number;
   onChange?: (value: number) => void;
 }
 
+const RateStyled = styled(Rate)`
+  width: 6rem;
+`;
+
 export function Rating({ defaultValue = 0, onChange }: RatingProps) {
   return (
-    <Rate
-      size="xs"
-      style={{ width: "6rem" }}
-      defaultValue={defaultValue}
-      onChange={onChange}
-    />
+    <RateStyled size="xs" defaultValue={defaultValue} onChange={onChange} />
   );
 }
