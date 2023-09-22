d7bc750b247cfdf76eca7152fdbf13212b113425
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-09-22 12:45:55 -0300

    fix: BodySecondary

--- src/components/typography/body-secondary/body-secondary.tsx
+++ src/components/typography/body-secondary/body-secondary.tsx
@@ -6,12 +6,12 @@ interface BodySecondaryProps {
   scratched?: boolean;
   color?: "brandCtaDark" | "baseGray";
 }
+
 export const BodySecondary = styled.p<BodySecondaryProps>`
   text-decoration: ${({ scratched }) => (scratched ? "line-through" : "")};
   font-family: ${constants.font.primary};
   font-size: ${constants.size.small};
   font-weight: ${constants.weight.regular};
   line-height: ${constants.lineHeight.large};
-  color: ${({ color }) =>
-    color === "brandCtaDark" ? colors.brandCtaDark : colors.baseGray};
+  color: ${({ color }) => colors[color || "baseGray"]};
 `;
