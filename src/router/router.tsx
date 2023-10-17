8cfca05cfd9dc95993a9ac835f69473b0bb6243c
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-10-17 20:07:39 -0300

    feat: add router login mobile

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "../pages/home/home";
+import { Login } from "../components/login/login";
+import { LoginMobile } from "../pages/login-mobile/login-mobile";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginMobile />,
   },
 ]);
 
