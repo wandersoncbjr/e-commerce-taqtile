f241fa9aa30343945c4c3995e32e165d4a36d550
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-10-22 00:41:03 -0300

    refactor(input): transform Input component into a functional component
    - Reimplemented the Input component as a functional component using the forwardRef hook.
    - Added ref support for Input component.
    - Improved code readability and maintainability.

--- src/components/inputs/Input/index.tsx
+++ src/components/inputs/Input/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useState, forwardRef } from "react";
 import { useId } from "react";
 import { InputHTMLAttributes } from "react";
 import {
@@ -19,35 +19,38 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   expanded?: boolean;
 }
 
-export function Input({ label, type, error, icon, ...rest }: InputProps) {
-  const labelId = useId();
-  const [isPasswordVisible, setPasswordVisible] = useState(false);
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, type, error, icon, ...rest }, ref) => {
+    const labelId = useId();
+    const [isPasswordVisible, setPasswordVisible] = useState(false);
 
-  const togglePasswordVisibility = () => {
-    setPasswordVisible((prev) => !prev);
-  };
+    const togglePasswordVisibility = () => {
+      setPasswordVisible((prev) => !prev);
+    };
 
-  return (
-    <WrapperInputStyled>
-      {label && (
-        <Label error={error} htmlFor={labelId}>
-          {label}
-        </Label>
-      )}
-      <SeparatorStyle size="small" />
-      <InputContainerStyled error={error}>
-        <InputStyle
-          type={isPasswordVisible ? "text" : type}
-          {...rest}
-          id={labelId}
-        />
-        {type === "password" ? (
-          <IconPassword onClick={togglePasswordVisibility} />
-        ) : null}
-        {icon}
-      </InputContainerStyled>
-      <SeparatorStyle size="small" />
-      {error && <Caption>{error}</Caption>}
-    </WrapperInputStyled>
-  );
-}
+    return (
+      <WrapperInputStyled>
+        {label && (
+          <Label error={error} htmlFor={labelId}>
+            {label}
+          </Label>
+        )}
+        <SeparatorStyle size="small" />
+        <InputContainerStyled error={error}>
+          <InputStyle
+            type={isPasswordVisible ? "text" : type}
+            {...rest}
+            id={labelId}
+            ref={ref}
+          />
+          {type === "password" ? (
+            <IconPassword onClick={togglePasswordVisibility} />
+          ) : null}
+          {icon}
+        </InputContainerStyled>
+        <SeparatorStyle size="small" />
+        {error && <Caption>{error}</Caption>}
+      </WrapperInputStyled>
+    );
+  }
+);
