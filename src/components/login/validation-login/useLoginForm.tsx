13bc8f981992ae500a753bbe4f5d414b84b66c9f
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-10-22 14:28:40 -0300

    refactor(login): separate schemaLogin and useLoginForm for better organization
    - Separated the schemaLogin for data validation rules.
    - Refactored useLoginForm to use schemaLogin for validation.
    - Improved code organization and separation of concerns.

--- src/components/login/validation-login/useLoginForm.tsx
+++ src/components/login/validation-login/useLoginForm.tsx
@@ -1,48 +1,17 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { regexPatterns } from "../../../regex/regex";
+import { schemaLogin } from "./schema-login";
 
-export const useLoginForm = () => {
-  type FormProps = z.infer<typeof schema>;
-  const schema = z.object({
-    email: z
-      .string()
-      .min(1, "O campo é obrigatório.")
-      .refine(
-        (value) => {
-          return (
-            regexPatterns.email.test(value) ||
-            regexPatterns.cpf.test(value) ||
-            regexPatterns.cnpj.test(value)
-          );
-        },
-        {
-          message: "Por favor, insira um email, CPF ou CNPJ válido.",
-        }
-      ),
-    password: z
-      .string()
-      .min(1, "A senha é obrigatória.")
-      .min(8, "A senha deve conter no mínimo 8 caracteres.")
-      .refine((value) => regexPatterns.specialCharacter.test(value), {
-        message: "A senha deve conter pelo menos um caractere especial.",
-      })
-      .refine((value) => regexPatterns.uppercaseLetter.test(value), {
-        message: "A senha deve conter pelo menos uma letra maiúscula.",
-      })
-      .refine((value) => regexPatterns.numericDigit.test(value), {
-        message: "A senha deve conter pelo menos um número.",
-      }),
-  });
-
+type FormProps = z.infer<typeof schemaLogin>;
+export function useLoginForm() {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<FormProps>({
     mode: "all",
-    resolver: zodResolver(schema),
+    resolver: zodResolver(schemaLogin),
   });
 
   const handleForm = (data: FormProps) => {};
@@ -53,4 +22,4 @@ export const useLoginForm = () => {
     errors,
     handleForm,
   };
-};
+}
