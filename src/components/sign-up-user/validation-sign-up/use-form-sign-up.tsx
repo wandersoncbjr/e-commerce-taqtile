7dca4a6e1d7370c5947d93225330307ff86b9d5b
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-10-27 19:09:44 -0300

    fix

--- src/components/sign-up-user/validation-sign-up/use-form-sign-up.tsx
+++ src/components/sign-up-user/validation-sign-up/use-form-sign-up.tsx
@@ -2,6 +2,9 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { schemaSignUp } from "./schema-sign-up";
+import { useNavigate } from "react-router";
+import { useMutation } from "@apollo/client";
+import { SIGN_UP_MUTATION } from "../../../graphql/mutations/mutarion-sign-up/mutarion-sign-up";
 
 type SignUpFormProps = z.infer<typeof schemaSignUp>;
 export function useSignUpForm() {
@@ -14,12 +17,31 @@ export function useSignUpForm() {
     resolver: zodResolver(schemaSignUp),
   });
 
-  const handleSignUp = (data: SignUpFormProps) => {};
+  const navigate = useNavigate();
+  const [signUp, { loading }] = useMutation(SIGN_UP_MUTATION, {
+    onCompleted: () => {
+      navigate("/login");
+    },
+  });
+
+  const handleSignUp = (data: SignUpFormProps) => {
+    signUp({
+      variables: {
+        data: {
+          email: data.email,
+          password: data.password,
+          name: data.name,
+        },
+      },
+    });
+  };
 
   return {
     register,
     handleSubmit,
     errors,
     handleSignUp,
+    loading,
+
   };
 }
