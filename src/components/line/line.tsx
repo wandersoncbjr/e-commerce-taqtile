f9925da62c1c9c28cbadecc734beb74b597ccf92
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-11-10 16:07:54 -0300

    style(Line): add styled component for horizontal line

---
+++ src/components/line/line.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { colors } from "../typography/colors";
+
+export const Line = styled.div`
+  height: 2px;
+  background-color: ${colors.baseGrayXlight};
+  width: 100%;
+`;
