757020d5dc158847547d337c4513f9719ef8a73d
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-11-13 20:32:27 -0300

    feat: add component card discount

---
+++ src/components/card-discount/card-discount.tsx
@@ -0,0 +1,11 @@
+import { InputButtonCard } from "../input-button-card/input-button-card";
+
+export function CardDiscount() {
+  return (
+    <InputButtonCard
+      title="Cupom de desconto"
+      placeholder="Inserir cupom"
+      titleButton="Adicionar"
+    ></InputButtonCard>
+  );
+}
