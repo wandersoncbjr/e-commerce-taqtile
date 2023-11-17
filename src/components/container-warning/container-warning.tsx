66e9cd50a3d0c9c60da58bc58f221e863ba0d907
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-11-17 18:40:39 -0300

    container warning

---
+++ src/components/container-warning/container-warning.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { colors } from "../typography/colors";
+
+export const ContainerWarning = styled.div`
+  background-color: ${colors.stateswarningLight};
+  padding: 8px 12px 8px 12px;
+  border-radius: 5px;
+`;
