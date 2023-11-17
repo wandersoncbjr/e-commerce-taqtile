a4b434d4c53bf73e5a9db517eee834e38196ea94
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-11-17 17:27:34 -0300

    feat: add component IconArrowLow

---
+++ src/assets/icons/icon-arrow-low.tsx
@@ -0,0 +1,19 @@
+export function IconArrowLow() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+    >
+      <path
+        d="M6 9.5L12 15.5L18 9.5"
+        stroke="#99A2AB"
+        stroke-width="1.25"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
